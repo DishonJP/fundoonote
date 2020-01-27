58fd0cfec225d7070043e689ad4b9f0e97b04e5b
DishonJP <dishonjp11@gmail.com>
2020-01-27 18:57:28 +0530

    [Dishon].added few contents to home page

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
-import { Button, Grid,Divider, AppBar, Toolbar } from '@material-ui/core'
+import { Typography, IconButton, AppBar, Toolbar } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
+        }
+    }
     render() {
         return (
             <div>
                 <AppBar position="static">
                     <Toolbar>
-                        
-            </Toolbar>
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="open drawer"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h4"
+                            noWrap
+                        >
+                            FundooNote
+          </Typography>
+                    </Toolbar>
                 </AppBar>
             </div>
         )
