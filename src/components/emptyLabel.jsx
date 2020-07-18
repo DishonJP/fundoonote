4d85fa038c8b15f1e441a335ce034406d0feb4ef
DishonJP <dishonjp11@gmail.com>
2020-07-18 16:32:33 +0530

    [Dishon].refactor emptyLabel to functional component

--- src/components/emptyLabel.jsx
+++ src/components/emptyLabel.jsx
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import label from '../assets/label.jpg'
 import { Typography } from '@material-ui/core'
-class EmptyLabel extends Component{
-    render() {
-        return (
-            <div style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                opacity:"0.8"
-            }}>
-                <img style={{
-                    height: "455px"
-                }} src={label} alt="lol"/>
-                <Typography variant="h4">No Notes are available in current label</Typography>
+
+const EmptyLabel = () => {
+    return (
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            opacity: "0.8"
+        }}>
+            <img style={{
+                height: "455px"
+            }} src={label} alt="lol" />
+            <Typography variant="h4">No Notes are available in current label</Typography>
         </div>
     )
 }
-}
+
 export default EmptyLabel
