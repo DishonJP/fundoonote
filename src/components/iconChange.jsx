2625ded3827d06fae74da7fc22795388e013ce48
DishonJP <dishonjp11@gmail.com>
2020-07-18 16:31:27 +0530

    [Dishon].refactor iconChange to functional component

--- src/components/iconChange.jsx
+++ src/components/iconChange.jsx
@@ -1,21 +1,21 @@
-import React, { Component } from "react";
-import { IconButton} from '@material-ui/core';
+import React from "react";
+import { IconButton } from '@material-ui/core';
 import ViewCompactIcon from '@material-ui/icons/ViewCompact';
 import ViewListIcon from '@material-ui/icons/ViewList';
-export default class IconChange extends Component{
-    render() {
-            return (
-                <IconButton
-                id="icon_butSize"
-                    onClick={this.props.change}>
-                    {(this.props.data)?<ViewCompactIcon id="font_sizeicon"
-                        fontSize="medium"
-                    />:
-                    <ViewListIcon id="font_sizeicon"
-                        fontSize="medium"
-                    />
-                    }
-                </IconButton>
-            )
+
+const IconChange = (props) => {
+    return (
+        <IconButton
+            id="icon_butSize"
+            onClick={props.change}>
+            {(this.props.data) ? <ViewCompactIcon id="font_sizeicon"
+                fontSize="medium"
+            /> :
+                <ViewListIcon id="font_sizeicon"
+                    fontSize="medium"
+                />
+            }
+        </IconButton>
+    )
 }
-}
+export default IconChange
